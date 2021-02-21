bf76dc0e0a30fd5393f9daf73c3cd3028c9579de
AndrewDaws <AndrewDaws@users.noreply.github.com>
2021-02-20 22:36:07 -0500

    Updated linking status messages.

--- scripts/create_links.sh
+++ scripts/create_links.sh
@@ -13,53 +13,53 @@ fi
 link_headless() {
   abort_file_dne "${HOME}/.dotfiles/zsh/.zshrc"
   if [[ "$(readlink -f "${HOME}/.zshrc")" == "${HOME}/.dotfiles/zsh/.zshrc" ]]; then
-    print_step "Skipped: ${HOME}/.zshrc"
+    print_step "Skipped: Linking ${HOME}/.zshrc"
   else
     if file_exists "${HOME}/.zshrc"; then
       rm -f "${HOME}/.zshrc"
     fi
+    print_step "Linking ${HOME}/.zshrc -> ${HOME}/.dotfiles/zsh/.zshrc"
     ln -s "${HOME}/.dotfiles/zsh/.zshrc" "${HOME}/.zshrc"
-    print_step "Linked: ${HOME}/.zshrc -> ${HOME}/.dotfiles/zsh/.zshrc"
   fi
 
   abort_file_dne "${HOME}/.dotfiles/zsh/.p10k.zsh"
   if [[ "$(readlink -f "${HOME}/.p10k.zsh")" == "${HOME}/.dotfiles/zsh/.p10k.zsh" ]]; then
-    print_step "Skipped: ${HOME}/.p10k.zsh"
+    print_step "Skipped: Linking ${HOME}/.p10k.zsh"
   else
     if file_exists "${HOME}/.p10k.zsh"; then
       rm -f "${HOME}/.p10k.zsh"
     fi
+    print_step "Linking ${HOME}/.p10k.zsh -> ${HOME}/.dotfiles/zsh/.p10k.zsh"
     ln -s "${HOME}/.dotfiles/zsh/.p10k.zsh" "${HOME}/.p10k.zsh"
-    print_step "Linked: ${HOME}/.p10k.zsh -> ${HOME}/.dotfiles/zsh/.p10k.zsh"
   fi
 
   abort_file_dne "${HOME}/.dotfiles/tmux/.tmux.conf"
   if [[ "$(readlink -f "${HOME}/.tmux.conf")" == "${HOME}/.dotfiles/tmux/.tmux.conf" ]]; then
-    print_step "Skipped: ${HOME}/.tmux.conf"
+    print_step "Skipped: Linking ${HOME}/.tmux.conf"
   else
     if file_exists "${HOME}/.tmux.conf"; then
       rm -f "${HOME}/.tmux.conf"
     fi
+    print_step "Linking ${HOME}/.tmux.conf -> ${HOME}/.dotfiles/tmux/.tmux.conf"
     ln -s "${HOME}/.dotfiles/tmux/.tmux.conf" "${HOME}/.tmux.conf"
-    print_step "Linked: ${HOME}/.tmux.conf -> ${HOME}/.dotfiles/tmux/.tmux.conf"
   fi
 
   abort_file_dne "${HOME}/.dotfiles/vim/.vimrc"
   if [[ "$(readlink -f "${HOME}/.vimrc")" == "${HOME}/.dotfiles/vim/.vimrc" ]]; then
-    print_step "Skipped: ${HOME}/.vimrc"
+    print_step "Skipped: Linking ${HOME}/.vimrc"
   else
     if file_exists "${HOME}/.vimrc"; then
       rm -f "${HOME}/.vimrc"
     fi
+    print_step "Linking ${HOME}/.vimrc -> ${HOME}/.dotfiles/vim/.vimrc"
     ln -s "${HOME}/.dotfiles/vim/.vimrc" "${HOME}/.vimrc"
-    print_step "Linked: ${HOME}/.vimrc -> ${HOME}/.dotfiles/vim/.vimrc"
   fi
 }
 
 link_desktop() {
   abort_file_dne "${HOME}/.dotfiles/gnome/gtk.css"
   if [[ "$(readlink -f "${HOME}/.config/gtk-3.0/gtk.css")" == "${HOME}/.dotfiles/gnome/gtk.css" ]]; then
-    print_step "Skipped: ${HOME}/.config/gtk-3.0/gtk.css"
+    print_step "Skipped: Linking ${HOME}/.config/gtk-3.0/gtk.css"
   else
     if file_exists "${HOME}/.config/gtk-3.0/gtk.css"; then
       rm -f "${HOME}/.config/gtk-3.0/gtk.css"
@@ -67,13 +67,13 @@ link_desktop() {
       mkdir -p "${HOME}/.config/gtk-3.0"
       chmod 775 "${HOME}/.config/gtk-3.0"
     fi
+    print_step "Linking ${HOME}/.config/gtk-3.0/gtk.css -> ${HOME}/.dotfiles/gnome/gtk.css"
     ln -s "${HOME}/.dotfiles/gnome/gtk.css" "${HOME}/.config/gtk-3.0/gtk.css"
-    print_step "Linked: ${HOME}/.config/gtk-3.0/gtk.css -> ${HOME}/.dotfiles/gnome/gtk.css"
   fi
 
   abort_file_dne "${HOME}/.dotfiles/gnome/settings.ini"
   if [[ "$(readlink -f "${HOME}/.config/gtk-3.0/settings.ini")" == "${HOME}/.dotfiles/gnome/settings.ini" ]]; then
-    print_step "Skipped: ${HOME}/.config/gtk-3.0/settings.ini"
+    print_step "Skipped: Linking ${HOME}/.config/gtk-3.0/settings.ini"
   else
     if file_exists "${HOME}/.config/gtk-3.0/settings.ini"; then
       rm -f "${HOME}/.config/gtk-3.0/settings.ini"
@@ -81,13 +81,13 @@ link_desktop() {
       mkdir -p "${HOME}/.config/gtk-3.0"
       chmod 775 "${HOME}/.config/gtk-3.0"
     fi
+    print_step "Linking ${HOME}/.config/gtk-3.0/settings.ini -> ${HOME}/.dotfiles/gnome/settings.ini"
     ln -s "${HOME}/.dotfiles/gnome/settings.ini" "${HOME}/.config/gtk-3.0/settings.ini"
-    print_step "Linked: ${HOME}/.config/gtk-3.0/settings.ini -> ${HOME}/.dotfiles/gnome/settings.ini"
   fi
 
   abort_file_dne "${HOME}/.dotfiles/gnome/settings.ini"
   if [[ "$(readlink -f "${HOME}/.config/gtk-4.0/settings.ini")" == "${HOME}/.dotfiles/gnome/settings.ini" ]]; then
-    print_step "Skipped: ${HOME}/.config/gtk-4.0/settings.ini"
+    print_step "Skipped: Linking ${HOME}/.config/gtk-4.0/settings.ini"
   else
     if file_exists "${HOME}/.config/gtk-4.0/settings.ini"; then
       rm -f "${HOME}/.config/gtk-4.0/settings.ini"
@@ -95,14 +95,14 @@ link_desktop() {
       mkdir -p "${HOME}/.config/gtk-4.0"
       chmod 775 "${HOME}/.config/gtk-4.0"
     fi
+    print_step "Linking ${HOME}/.config/gtk-4.0/settings.ini -> ${HOME}/.dotfiles/gnome/settings.ini"
     ln -s "${HOME}/.dotfiles/gnome/settings.ini" "${HOME}/.config/gtk-4.0/settings.ini"
-    print_step "Linked: ${HOME}/.config/gtk-4.0/settings.ini -> ${HOME}/.dotfiles/gnome/settings.ini"
   fi
 
   if [[ -d "${HOME}/.local/share/fonts/OperatorMono" ]]; then
     abort_file_dne "${HOME}/.dotfiles/alacritty/alacritty-operatormono.yml"
     if [[ "$(readlink -f "${HOME}/.config/alacritty/alacritty.yml")" == "${HOME}/.dotfiles/alacritty/alacritty-operatormono.yml" ]]; then
-      print_step "Skipped: ${HOME}/.config/alacritty/alacritty.yml"
+      print_step "Skipped: Linking ${HOME}/.config/alacritty/alacritty.yml"
     else
       if file_exists "${HOME}/.config/alacritty/alacritty.yml"; then
         rm -f "${HOME}/.config/alacritty/alacritty.yml"
@@ -110,13 +110,13 @@ link_desktop() {
         mkdir -p "${HOME}/.config/alacritty"
         chmod 700 "${HOME}/.config/alacritty"
       fi
+      print_step "Linking ${HOME}/.config/alacritty/alacritty.yml -> ${HOME}/.dotfiles/alacritty/alacritty-operatormono.yml"
       ln -s "${HOME}/.dotfiles/alacritty/alacritty-operatormono.yml" "${HOME}/.config/alacritty/alacritty.yml"
-      print_step "Linked: ${HOME}/.config/alacritty/alacritty.yml -> ${HOME}/.dotfiles/alacritty/alacritty-operatormono.yml"
     fi
   elif [[ -d "${HOME}/.local/share/fonts/JetBrains" ]]; then
     abort_file_dne "${HOME}/.dotfiles/alacritty/alacritty-jetbrainsmono.yml"
     if [[ "$(readlink -f "${HOME}/.config/alacritty/alacritty.yml")" == "${HOME}/.dotfiles/alacritty/alacritty-jetbrainsmono.yml" ]]; then
-      print_step "Skipped: ${HOME}/.config/alacritty/alacritty.yml"
+      print_step "Skipped: Linking ${HOME}/.config/alacritty/alacritty.yml"
     else
       if file_exists "${HOME}/.config/alacritty/alacritty.yml"; then
         rm -f "${HOME}/.config/alacritty/alacritty.yml"
@@ -124,13 +124,13 @@ link_desktop() {
         mkdir -p "${HOME}/.config/alacritty"
         chmod 700 "${HOME}/.config/alacritty"
       fi
+      print_step "Linking ${HOME}/.config/alacritty/alacritty.yml -> ${HOME}/.dotfiles/alacritty/alacritty-jetbrainsmono.yml"
       ln -s "${HOME}/.dotfiles/alacritty/alacritty-jetbrainsmono.yml" "${HOME}/.config/alacritty/alacritty.yml"
-      print_step "Linked: ${HOME}/.config/alacritty/alacritty.yml -> ${HOME}/.dotfiles/alacritty/alacritty-jetbrainsmono.yml"
     fi
   else
     abort_file_dne "${HOME}/.dotfiles/alacritty/alacritty-firacode.yml"
     if [[ "$(readlink -f "${HOME}/.config/alacritty/alacritty.yml")" == "${HOME}/.dotfiles/alacritty/alacritty-firacode.yml" ]]; then
-      print_step "Skipped: ${HOME}/.config/alacritty/alacritty.yml"
+      print_step "Skipped: Linking ${HOME}/.config/alacritty/alacritty.yml"
     else
       if file_exists "${HOME}/.config/alacritty/alacritty.yml"; then
         rm -f "${HOME}/.config/alacritty/alacritty.yml"
@@ -138,8 +138,8 @@ link_desktop() {
         mkdir -p "${HOME}/.config/alacritty"
         chmod 700 "${HOME}/.config/alacritty"
       fi
+      print_step "Linking ${HOME}/.config/alacritty/alacritty.yml -> ${HOME}/.dotfiles/alacritty/alacritty-firacode.yml"
       ln -s "${HOME}/.dotfiles/alacritty/alacritty-firacode.yml" "${HOME}/.config/alacritty/alacritty.yml"
-      print_step "Linked: ${HOME}/.config/alacritty/alacritty.yml -> ${HOME}/.dotfiles/alacritty/alacritty-firacode.yml"
     fi
   fi
 }
