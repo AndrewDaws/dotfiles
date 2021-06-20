8df44bea93dea18bc16435487ed196cc77f11153
AndrewDaws <AndrewDaws@users.noreply.github.com>
2021-06-20 15:10:07 -0400

    Fzf-tab improvements

--- zsh/.fzf-tab.zsh
+++ zsh/.fzf-tab.zsh
@@ -1,6 +1,11 @@
+# Zsh options
 # disable sort when completing `git checkout`
 zstyle ':completion:*:git-checkout:*' sort false
-# preview directory's content with exa when completing cd
-zstyle ':fzf-tab:complete:cd:*' fzf-preview 'exa -1 --color=always $realpath'
+
+# Fzf-tab options
 # switch group using `,` and `.`
 zstyle ':fzf-tab:*' switch-group ',' '.'
+
+# Completions
+# preview directory's content with exa when completing cd
+zstyle ':fzf-tab:complete:cd:*' fzf-preview 'exa --color=always --color-scale --git --classify --icons --tree --level=1 ${realpath}'
