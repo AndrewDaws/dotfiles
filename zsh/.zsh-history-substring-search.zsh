bc8b8409b079c5ad193f9d181cd6a1c3d361e772
AndrewDaws <AndrewDaws@users.noreply.github.com>
2021-06-20 10:49:40 -0400

    Updated history substring search keybinds to be more reliable

--- zsh/.zsh-history-substring-search.zsh
+++ zsh/.zsh-history-substring-search.zsh
@@ -5,5 +5,8 @@ export HISTORY_SUBSTRING_SEARCH_ENSURE_UNIQUE="true"
 export HISTORY_SUBSTRING_SEARCH_FUZZY="true"
 
 # Enable arrow keys up and down to go through matching command history
+zmodload zsh/terminfo
+bindkey "$terminfo[kcuu1]" history-substring-search-up
+bindkey "$terminfo[kcud1]" history-substring-search-down
 bindkey '^[[A' history-substring-search-up
 bindkey '^[[B' history-substring-search-down
