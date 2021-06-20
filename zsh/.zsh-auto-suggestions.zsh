697eec8262a29577c297386bf0623f0a50b959d5
AndrewDaws <AndrewDaws@users.noreply.github.com>
2021-06-20 17:59:07 -0400

    Significant paste speed-up/fix when using zsh-auto-suggestions

--- zsh/.zsh-auto-suggestions.zsh
+++ zsh/.zsh-auto-suggestions.zsh
@@ -3,3 +3,18 @@ export ZSH_AUTOSUGGEST_STRATEGY=(match_prev_cmd history completion)
 
 # Make suggestion lookup async
 export ZSH_AUTOSUGGEST_USE_ASYNC="true"
+
+# This speeds up pasting
+# https://github.com/zsh-users/zsh-autosuggestions/issues/238
+pasteinit() {
+  OLD_SELF_INSERT=${${(s.:.)widgets[self-insert]}[2,3]}
+  zle -N self-insert url-quote-magic
+}
+
+pastefinish() {
+  zle -N
+  self-insert $OLD_SELF_INSERT
+}
+
+zstyle :bracketed-paste-magic paste-init pasteinit
+zstyle :bracketed-paste-magic paste-finish pastefinish
