94a3488c7f75da590cba1827d7bd569e4baeaf29
AndrewDaws <AndrewDaws@users.noreply.github.com>
2021-06-21 09:27:27 -0400

    Added cat/bat/less/more fzf-tab previews

--- zsh/.fzf-tab.zsh
+++ zsh/.fzf-tab.zsh
@@ -9,3 +9,4 @@ zstyle ':fzf-tab:*' switch-group ',' '.'
 # Completions
 # preview directory's content with exa when completing cd
 zstyle ':fzf-tab:complete:cd:*' fzf-preview 'exa --color=always --color-scale --git --classify --icons --tree --level=1 ${realpath}'
+zstyle ':fzf-tab:complete:(cat|bat|less|more):*' fzf-preview '[[ -d ${realpath} ]] && exa --color=always --color-scale --git --classify --icons --tree --level=1 ${realpath} || bat --style=numbers --color=always ${realpath} | head -500'
