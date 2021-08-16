371b1b01053ff4b5120327101cf51dc875b475d2
AndrewDaws <AndrewDaws@users.noreply.github.com>
2021-08-16 07:38:03 -0400

    Simplified Exa aliases

--- zsh/.exa.zsh
+++ zsh/.exa.zsh
@@ -9,352 +9,77 @@ alias exa="\exa \
   "
 
 # Aliases
-l() {
-  if [[ -n "${1}" ]]; then
-    if [[ "${1: -1}" == "/" ]]; then
-      exa \
-        --git-ignore \
-        --long \
-        -- "${1}"
-    elif [[ -d "${1}" ]]; then
-      exa \
-        --git-ignore \
-        --long \
-        -- "${1}"/
-    elif [[ "${1}" =~ "/" ]]; then
-      exa \
-        --git-ignore \
-        --long \
-        -- "$(dirname "${1}")"/*"$(basename "${1}")"*
-    else
-      exa \
-        --git-ignore \
-        --long \
-        -- *"${1}"*
-    fi
-  else
-    exa \
-      --git-ignore \
-      --long \
-      -- .
-  fi
-}
 
-la() {
-  if [[ -n "${1}" ]]; then
-    if [[ "${1: -1}" == "/" ]]; then
-      exa \
-        --all \
-        --long \
-        -- "${1}"
-    elif [[ -d "${1}" ]]; then
-      exa \
-        --all \
-        --long \
-        -- "${1}"/
-    elif [[ "${1}" =~ "/" ]]; then
-      exa \
-        --all \
-        --long \
-        -- "$(dirname "${1}")"/*"$(basename "${1}")"*
-    else
-      exa \
-        --all \
-        --long \
-        -- *"${1}"*
-    fi
-  else
-    exa \
-      --all \
-      --long \
-      -- .
-  fi
-}
+# List files and directories
+alias l="exa \
+  --git-ignore \
+  --long \
+  "
 
-lg() {
-  if [[ -n "${1}" ]]; then
-    if [[ "${1: -1}" == "/" ]]; then
-      exa \
-        --git-ignore \
-        --group \
-        --long \
-        -- "${1}"
-    elif [[ -d "${1}" ]]; then
-      exa \
-        --git-ignore \
-        --group \
-        --long \
-        -- "${1}"/
-    elif [[ "${1}" =~ "/" ]]; then
-      exa \
-        --git-ignore \
-        --group \
-        --long \
-        -- "$(dirname "${1}")"/*"$(basename "${1}")"*
-    else
-      exa \
-        --git-ignore \
-        --group \
-        --long \
-        -- *"${1}"*
-    fi
-  else
-    exa \
-      --git-ignore \
-      --group \
-      --long \
-      -- .
-  fi
-}
+# List all files and directories (including hidden)
+alias la="exa \
+  --all \
+  --long \
+  "
 
-lga() {
-  if [[ -n "${1}" ]]; then
-    if [[ "${1: -1}" == "/" ]]; then
-      exa \
-        --all \
-        --group \
-        --long \
-        -- "${1}"
-    elif [[ -d "${1}" ]]; then
-      exa \
-        --all \
-        --group \
-        --long \
-        -- "${1}"/
-    elif [[ "${1}" =~ "/" ]]; then
-      exa \
-        --all \
-        --group \
-        --long \
-        -- "$(dirname "${1}")"/*"$(basename "${1}")"*
-    else
-      exa \
-        --all \
-        --group \
-        --long \
-        -- *"${1}"*
-    fi
-  else
-    exa \
-      --all \
-      --group \
-      --long \
-      -- .
-  fi
-}
+# TODO Fix Exa File Aliases To Not Show Directories
+# List files
+alias lf="exa \
+  --git-ignore \
+  --long \
+  "
+# -l -- *(-.)
 
+# List all files (including hidden)
+alias lfa="exa \
+  --all \
+  --long \
+  "
+
+# List directories
+alias ld="exa \
+  --git-ignore \
+  --long \
+  --only-dirs \
+  "
+
+# List all directories (including hidden)
+alias lda="exa \
+  --all \
+  --long \
+  --only-dirs \
+  "
+
+# List files and directories with groups
+alias lg="exa \
+  --git-ignore \
+  --group \
+  --long \
+  "
+
+# List files and directories with groups (including hidden)
+alias lga="exa \
+  --all \
+  --group \
+  --long \
+  "
+
+# List ten most recently modified files and directories
 lt() {
-  if [[ -n "${1}" ]]; then
-    if [[ "${1: -1}" == "/" ]]; then
-      exa \
-        --git-ignore \
-        --long \
-        --reverse \
-        --sort=newest \
-        -- "${1}" | head -10
-    elif [[ -d "${1}" ]]; then
-      exa \
-        --git-ignore \
-        --long \
-        --reverse \
-        --sort=newest \
-        -- "${1}"/ | head -10
-    elif [[ "${1}" =~ "/" ]]; then
-      exa \
-        --git-ignore \
-        --long \
-        --reverse \
-        --sort=newest \
-        -- "$(dirname "${1}")"/*"$(basename "${1}")"* | head -10
-    else
-      exa \
-        --git-ignore \
-        --long \
-        --reverse \
-        --sort=newest \
-        -- *"${1}"* | head -10
-    fi
-  else
-    exa \
-      --git-ignore \
-      --long \
-      --reverse \
-      --sort=newest \
-      -- . | head -10
-  fi
+  exa \
+    --git-ignore \
+    --long \
+    --reverse \
+    --sort=newest \
+    "${*}" | head -10
 }
 
+# List ten most recently modified files and directories (including hidden)
 lta() {
-  if [[ -n "${1}" ]]; then
-    if [[ "${1: -1}" == "/" ]]; then
-      exa \
-        --all \
-        --long \
-        --reverse \
-        --sort=newest \
-        -- "${1}" | head -10
-    elif [[ -d "${1}" ]]; then
-      exa \
-        --all \
-        --long \
-        --reverse \
-        --sort=newest \
-        -- "${1}"/ | head -10
-    elif [[ "${1}" =~ "/" ]]; then
-      exa \
-        --all \
-        --long \
-        --reverse \
-        --sort=newest \
-        -- "$(dirname "${1}")"/*"$(basename "${1}")"* | head -10
-    else
-      exa \
-        --all \
-        --long \
-        --reverse \
-        --sort=newest \
-        -- *"${1}"* | head -10
-    fi
-  else
-    exa \
-      --all \
-      --long \
-      --reverse \
-      --sort=newest \
-      -- . | head -10
-  fi
-}
-
-lf() {
-  if [[ -n "${1}" ]]; then
-    if [[ "${1: -1}" == "/" ]]; then
-      exa \
-        --git-ignore \
-        --long \
-        -- "${1}"*(-.)
-    elif [[ -d "${1}" ]]; then
-      exa \
-        --git-ignore \
-        --long \
-        -- "${1}"/*(-.)
-    elif [[ "${1}" =~ "/" ]]; then
-      exa \
-        --git-ignore \
-        --long \
-        -- "$(dirname "${1}")"/*"$(basename "${1}")"*(-.)
-    else
-      exa \
-        --git-ignore \
-        --long \
-        -- *"${1}"*(-.)
-    fi
-  else
-    exa \
-      --git-ignore \
-      --long \
-      -- *(-.)
-  fi
-}
-
-lfa() {
-  if [[ -n "${1}" ]]; then
-    if [[ "${1: -1}" == "/" ]]; then
-      exa \
-        --all \
-        --long \
-        -- "${1}"*(D-.)
-    elif [[ -d "${1}" ]]; then
-      exa \
-        --all \
-        --long \
-        -- "${1}"/*(D-.)
-    elif [[ "${1}" =~ "/" ]]; then
-      exa \
-        --all \
-        --long \
-        -- "$(dirname "${1}")"/*"$(basename "${1}")"*(D-.)
-    else
-      exa \
-        --all \
-        --long \
-        -- *"${1}"*(D-.)
-    fi
-  else
-    exa \
-      --all \
-      --long \
-      -- *(D-.)
-  fi
-}
-
-ld() {
-  if [[ -n "${1}" ]]; then
-    if [[ "${1: -1}" == "/" ]]; then
-      exa \
-        --git-ignore \
-        --long \
-        --only-dirs \
-        -- "${1}"*(-/)
-    elif [[ -d "${1}" ]]; then
-      exa \
-        --git-ignore \
-        --long \
-        --only-dirs \
-        -- "${1}"/*(-/)
-    elif [[ "${1}" =~ "/" ]]; then
-      exa \
-        --git-ignore \
-        --long \
-        --only-dirs \
-        -- "$(dirname "${1}")"/*"$(basename "${1}")"*(-/)
-    else
-      exa \
-        --git-ignore \
-        --long \
-        --only-dirs \
-        -- *"${1}"*(-/)
-    fi
-  else
-    exa \
-      --git-ignore \
-      --long \
-      --only-dirs \
-      -- "${PWD}"
-  fi
-}
-
-lda() {
-  if [[ -n "${1}" ]]; then
-    if [[ "${1: -1}" == "/" ]]; then
-      exa \
-        --all \
-        --long \
-        --only-dirs \
-        -- "${1}"*(D-/)
-    elif [[ -d "${1}" ]]; then
-      exa \
-        --all \
-        --long \
-        --only-dirs \
-        -- "${1}"/*(D-/)
-    elif [[ "${1}" =~ "/" ]]; then
-      exa \
-        --all \
-        --long \
-        --only-dirs \
-        -- "$(dirname "${1}")"/*"$(basename "${1}")"*(D-/)
-    else
-      exa \
-        --all \
-        --long \
-        --only-dirs \
-        -- *"${1}"*(D-/)
-    fi
-  else
-    exa \
-      --all \
-      --long \
-      --only-dirs \
-      -- "${PWD}"
-  fi
+  exa \
+    --all \
+    --long \
+    --reverse \
+    --sort=newest \
+    "${*}" | head -10
 }
