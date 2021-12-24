41f21bdbaac63abca9c0c5e44774bdcb466c4876
Andrew Daws <AndrewDaws@users.noreply.github.com>
2021-12-23 21:09:45 -0500

    Added Bitwarden CLI to the Bitwarden install script

--- scripts/install_bitwarden.sh
+++ scripts/install_bitwarden.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo '=> Installing Bitwarden'
-echo 'Getting latest release from GitHub.'
+echo 'Getting latest desktop release from GitHub.'
 rm -f "${PWD}"/Bitwarden-*-amd64.deb
 curl -s https://api.github.com/repos/bitwarden/desktop/releases/latest |
     grep browser_download_url |
@@ -11,3 +11,16 @@ curl -s https://api.github.com/repos/bitwarden/desktop/releases/latest |
     wget -i -
 sudo apt install "${PWD}"/Bitwarden-*-amd64.deb
 rm -f "${PWD}"/Bitwarden-*-amd64.deb
+
+echo 'Getting latest CLI release from GitHub.'
+rm -f "${PWD}"/bw-linux-*.zip
+curl -s https://api.github.com/repos/bitwarden/cli/releases/latest |
+    grep browser_download_url |
+    grep bw-linux- |
+    grep .zip |
+    cut -d '"' -f 4 |
+    wget -i -
+mkdir -p "${HOME}/bin"
+unzip -o "${PWD}"/bw-linux-*.zip -d "${HOME}/bin"
+chmod +x "${HOME}/bin/bw"
+rm -f "${PWD}"/bw-linux-*.zip
