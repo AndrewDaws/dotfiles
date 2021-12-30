6a4e77534a7bfc6ac7fe8f1d2e77c4848834c66b
Andrew Daws <AndrewDaws@users.noreply.github.com>
2021-12-30 00:16:11 -0500

    Updated Wine install script to latest from WineHQ

--- scripts/install_wine.sh
+++ scripts/install_wine.sh
@@ -6,8 +6,15 @@ echo '=> Installing Wine'
 sudo dpkg --add-architecture i386
 sudo apt update
 
+# Import WineHQ repository’s GPG key
+wget -qO- https://dl.winehq.org/wine-builds/winehq.key | sudo apt-key add -
+
+# Add WineHQ repository
+sudo apt install software-properties-common
+sudo apt-add-repository "deb http://dl.winehq.org/wine-builds/ubuntu/ $(lsb_release -cs) main"
+
 # Install Wine
-sudo apt install wine64 wine32
+sudo apt install --install-recommends winehq-stable
 
 # Configure Wine
 winecfg
