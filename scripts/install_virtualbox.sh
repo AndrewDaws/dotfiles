e53c6b3edd03baf8e5844aef97bcb4a81f381454
Andrew Daws <AndrewDaws@users.noreply.github.com>
2022-03-27 20:22:19 -0400

    Added host-only networks configuration to VirtualBox script

--- scripts/install_virtualbox.sh
+++ scripts/install_virtualbox.sh
@@ -2,4 +2,11 @@
 
 echo '=> Installing VirtualBox'
 sudo apt install virtualbox virtualbox-ext-pack
-sudo usermod -a -G vboxusers "${USER}"
+sudo usermod --append --groups vboxusers "${USER}"
+sudo mkdir --parents -- "/etc/vbox"
+if ! grep --max-count=1 -- "\* 10.0.0.0/8 192.168.0.0/16" "/etc/vbox/networks.conf" &>/dev/null; then
+  echo "* 10.0.0.0/8 192.168.0.0/16" | sudo tee --append "/etc/vbox/networks.conf"
+fi
+if ! grep --max-count=1 -- "\* 2001::/64" "/etc/vbox/networks.conf" &>/dev/null; then
+  echo "* 2001::/64" | sudo tee --append "/etc/vbox/networks.conf"
+fi
