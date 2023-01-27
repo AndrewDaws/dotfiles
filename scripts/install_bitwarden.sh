62dd0838823ab05f91e71fdfd58c9bd8ac5dd012
Andrew Daws <AndrewDaws@users.noreply.github.com>
2023-01-27 16:59:50 -0500

    Updated Bitwarden install script with new repo

--- scripts/install_bitwarden.sh
+++ scripts/install_bitwarden.sh
@@ -3,10 +3,10 @@
 echo '=> Installing Bitwarden'
 echo 'Getting latest desktop release from GitHub.'
 rm -f "${PWD}"/Bitwarden-*-amd64.deb
-curl -s https://api.github.com/repos/bitwarden/desktop/releases/latest |
+curl -s https://api.github.com/repos/bitwarden/clients/releases |
     grep browser_download_url |
     grep Bitwarden- |
-    grep \\-amd64.deb |
+    grep --max-count=1 -- \\-amd64.deb |
     cut -d '"' -f 4 |
     wget -i -
 sudo apt install "${PWD}"/Bitwarden-*-amd64.deb
@@ -14,13 +14,18 @@ rm -f "${PWD}"/Bitwarden-*-amd64.deb
 
 echo 'Getting latest CLI release from GitHub.'
 rm -f "${PWD}"/bw-linux-*.zip
-curl -s https://api.github.com/repos/bitwarden/cli/releases/latest |
+curl -s https://api.github.com/repos/bitwarden/clients/releases |
     grep browser_download_url |
     grep bw-linux- |
-    grep .zip |
+    grep --max-count=1 -- .zip |
     cut -d '"' -f 4 |
     wget -i -
 mkdir -p "${HOME}/bin"
 unzip -o "${PWD}"/bw-linux-*.zip -d "${HOME}/bin"
 chmod +x "${HOME}/bin/bw"
 rm -f "${PWD}"/bw-linux-*.zip
+# @todo Add Bitwarden CLI Shell Profile
+# @body Create environment variables such as BW_SESSION
+
+# @todo Add Bitwarden CLI SSH Agent
+# @body https://github.com/joaojacome/bitwarden-ssh-agent
